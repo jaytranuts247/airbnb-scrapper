d7c236880517f3912ff60eda133829388db95c77
Jay Tran <jaytran@Jays-MacBook-Air.local>
2021-05-19 23:52:13 -0400

    change to use pupeteer

--- scrapper.js
+++ scrapper.js
@@ -1,22 +1,22 @@
-const chromium = require("chrome-aws-lambda");
-const puppeteer = require("puppeteer-core");
+// const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer");
 
 module.exports = function (url) {
   return new Promise((resolve, reject) => {
     (async () => {
-      // const browser = await puppeteer.launch({
-      //   // headless: true, // debug only
-      //   args: ["--no-sandbox"],
-      // });
-      const executablePath = await chromium.executablePath;
-
-      // setup
-      browser = await chromium.puppeteer.launch({
-        args: chromium.args,
-        executablePath: executablePath,
-        headless: chromium.headless,
-        defaultViewport: chromium.defaultViewport,
+      const browser = await puppeteer.launch({
+        // headless: true, // debug only
+        args: ["--no-sandbox"],
       });
+      // const executablePath = await chromium.executablePath;
+
+      // // setup
+      // browser = await chromium.puppeteer.launch({
+      //   args: chromium.args,
+      //   executablePath: executablePath,
+      //   headless: chromium.headless,
+      //   defaultViewport: chromium.defaultViewport,
+      // });
 
       const page = await browser.newPage();
       page.setViewport({ width: 1280, height: 800 });
@@ -25,6 +25,7 @@ module.exports = function (url) {
         waitUntil: ["load", "networkidle0", "domcontentloaded"],
         timeout: 0,
       });
+      // await page.waitforselector
       const content = await page.evaluate(() => document.body.innerHTML);
 
       await browser.close();
